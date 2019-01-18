83f992b58b8c47f4c1d119ee29b11c92ebf95ec7
SarahDamDTU <s153659@student.dtu.dk>
2019-01-18 14:28:31 +0100

    Customer Service and web end points. (again)

--- system_tests/src/main/java/payment/networking/WebEndpoints.java
+++ system_tests/src/main/java/payment/networking/WebEndpoints.java
@@ -1,7 +1,7 @@
 package payment.networking;
 
 public enum WebEndpoints {
-  BASE("http://127.0.0.1:8585");
+  BASE("http://127.0.0.1:8585"), BASEC("http://127.0.0.1:8686");
   //BASE("http://02267-munich.compute.dtu.dk:8585");
 
   public final String url;
