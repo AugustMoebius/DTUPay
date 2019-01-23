01c23d79383a2fd78fc29e513a7263fdb0613c69
SarahDamDTU <s153659@student.dtu.dk>
2019-01-23 11:15:50 +0100

    One author tags

--- system_tests/src/test/java/services/CvrService.java
+++ system_tests/src/test/java/services/CvrService.java
@@ -2,6 +2,9 @@ package services;
 
 import com.mifmif.common.regex.Generex;
 
+/**
+ * @author Sebastian
+ */
 public class CvrService {
   public static String generateCvr() {
     String cprRegex = "DK[0-9]{8}";
